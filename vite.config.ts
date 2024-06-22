7c14e17e42abd9c2b0f28078b4606e77b5d90031
EdMorales2986 <ed.morales6892@gmail.com>
2024-06-22 04:34:33 -0400

    Project Start

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import viteReact from "@vitejs/plugin-react"; Causes problems apperently because it acts like react...
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
+const isTest = process.env.NODE_ENV === "test";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    // viteReact(), // Guide says to use this, but it's problematic and apparently not needed according to the example repo...
+    !isTest && TanStackRouterVite(),
+  ],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+  },
+});
