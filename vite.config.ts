ee626d94787eb4145faa13e3e139189fadb6398d
EdMorales2986 <ed.morales6892@gmail.com>
2024-06-24 20:56:36 -0400

    Checkpoint 2.1

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import vercel from "vite-plugin-vercel";
 
 const isTest = process.env.NODE_ENV === "test";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel(), !isTest && TanStackRouterVite()],
+  plugins: [react(), !isTest && TanStackRouterVite()],
   test: {
     environment: "jsdom",
     setupFiles: "./src/tests/setup.ts",
